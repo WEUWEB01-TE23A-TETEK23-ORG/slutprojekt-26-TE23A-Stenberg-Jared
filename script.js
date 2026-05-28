e5fd402c24f29d98de07d3671095090928d55f70
Jared Stenberg <jared.stenberg@elev.ga.ntig.se>
2026-05-28 22:15:52 +0200

    fixade några små saker

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Funktion för när användaren klickar "Ja"
 function accepteraKakor() {
-    alert("Tack för att du har accepterat kakor🍪 Välkommen in till Gamer Room!");
+    alert("Välkommen in till Gamer Room! Här får du en kaka 🍪");
     // Döljer hela cookie-rutan så att man kan använda sidan
     document.getElementById("cookie-ruta").style.display = "none";
 }
 
 // Funktion för när användaren klickar "Nej"
 function nekaKakor() {
-    alert("DÅ blir det inga kakor för dig, hejdå:(");
+    alert("Då blir det inga kakor till dig, hejdå:(");
     // Skickar iväg användaren till Google
     window.location.href = "https://www.google.com"; 
 }
