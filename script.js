96e7afdbbbaaa9536828bdf189a8ca35cb99990f
Jared Stenberg <jared.stenberg@elev.ga.ntig.se>
2026-06-01 10:26:39 +0200

    Ändrade bilderna till AI genererade bilder eftersom att bilderna som användes innan var tagna direkt från google. Ändrade också några få stavfel och ordval

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 // Funktion för när användaren klickar "Ja"
 function accepteraKakor() {
-    alert("Välkommen in till Gamer Room! Här får du en kaka 🍪");
-    // Döljer hela cookie-rutan så att man kan använda sidan
+    alert("Välkommen in till Gamer Room! Här får du en kaka🍪");
+    
+    // Gömmer cookie-rutan genom att ändra dess CSS-stil till "none"
     document.getElementById("cookie-ruta").style.display = "none";
 }
 
 // Funktion för när användaren klickar "Nej"
 function nekaKakor() {
-    alert("Då blir det inga kakor till dig, hejdå:(");
+    alert("Då blir det inga kakor för dig, hejdå:(");
+    
     // Skickar iväg användaren till Google
     window.location.href = "https://www.google.com"; 
 }
